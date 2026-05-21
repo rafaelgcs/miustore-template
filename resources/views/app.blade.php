3b7eb81869a840295d61a86664016919e95cc471
Rafael Guimarães <contato@rafaelgcs.com.br>
2026-05-20 22:32:13 -0300

    feat: add new favicon image and update existing favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <link rel="icon" type="image/jpeg" href="{{ asset('favicon.jpg') }}" />
+        <link rel="shortcut icon" type="image/x-icon" href="{{ asset('favicon.ico') }}" />
+
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
